19c7b7acd8960b2e7bd5b5d08af248a8af8375ae
abhinan2058 <chyaavinan123@gmail.com>
2023-09-28 14:03:20 +0545

    Deployed

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,22 @@
-module.exports = {
-    HOST: "localhost",
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "spt",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   };
+
+  module.exports = {
+    HOST: "containers-us-west-49.railway.app",
     USER: "root",
-    PASSWORD: "",
-    DB: "spt",
+    PASSWORD: "Vtf3RC3ZZQhWRIU9VCu9",
+    DB: "railway",
     dialect: "mysql",
     pool: {
       max: 5,
--- model/index.js
+++ model/index.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
+  port:5688,
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
